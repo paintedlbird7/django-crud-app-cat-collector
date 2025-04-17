1525b97e4f642dedf92af11651749ce2b5bf6168
Rosa Perez <rosaperez@Rosas-MacBook-Pro.local>
2025-04-17 14:54:53 -0700

    added cat objects into db

--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,32 @@
 from django.shortcuts import render
 
+# views.py
+
+class Cat:
+    def __init__(self, name, breed, description, age):
+        self.name = name
+        self.breed = breed
+        self.description = description
+        self.age = age
+
+# Create a list of Cat instances
+cats = [
+    Cat('Lolo', 'tabby', 'Kinda rude.', 3),
+    Cat('Sachi', 'tortoiseshell', 'Looks like a turtle.', 0),
+    Cat('Fancy', 'bombay', 'Happy fluff ball.', 4),
+    Cat('Bonk', 'selkirk rex', 'Meows loudly.', 6)
+]
+
+# views.py
+
+def cat_index(request):
+    # Render the cats/index.html template with the cats data
+    return render(request, 'cats/index.html', {'cats': cats})
+
+
 # Create your views here.
 # Import HttpResponse to send text-based responses
-from django.http import HttpResponse
+# from django.http import HttpResponse
 
 # Define the home view function
 def home(request):
@@ -14,4 +38,8 @@ def about(request):
 
 def about(request):
     return render(request, 'about.html')
+
+def home(request):
+    return render(request, 'home.html')
+
 
