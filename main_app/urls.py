0435c3bb7585672382c81de3cb4be3fa5cca528f
Rosa Perez <rosaperez@Rosas-MacBook-Pro.local>
2025-04-16 21:35:20 -0700

    renders hello with the cat

---
+++ main_app/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views # Import views to connect routes to view functions
+
+urlpatterns = [
+    # Routes will be added here
+    path('', views.home, name='home'),
+]
--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+# Import HttpResponse to send text-based responses
+from django.http import HttpResponse
+
+# Define the home view function
+def home(request):
+    # Send a simple HTML response
+    return HttpResponse('<h1>Hello ᓚᘏᗢ</h1>')
+
