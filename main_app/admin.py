a2bc1c11e1894d798cb7051ff6789d778f3177cb
Rosa Perez <rosaperez@Rosas-MacBook-Pro.local>
2025-04-18 12:55:06 -0700

    wip: added feedings to details page

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
-from .models import Cat
+# Add Feeding to the import
+from .models import Cat, Feeding
 
 admin.site.register(Cat)
+# Register the new Feeding model
+admin.site.register(Feeding)
--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 from django.urls import reverse
 
+# A tuple of 2-tuples added above our models
+MEALS = (
+    ('B', 'Breakfast'),
+    ('L', 'Lunch'),
+    ('D', 'Dinner')
+)
+
 class Cat(models.Model):
     name = models.CharField(max_length=100)
     breed = models.CharField(max_length=100)
@@ -14,3 +21,22 @@ class Cat(models.Model):
     def get_absolute_url(self):
         # Use the 'reverse' function to dynamically find the URL for viewing this cat's details
         return reverse('cat-detail', kwargs={'cat_id': self.id})
+
+# Add new Feeding model below Cat model
+class Feeding(models.Model):
+    # date = models.DateField()
+    # The first optional positional argument overrides the label
+    date = models.DateField('Feeding date')
+    meal = models.CharField(
+        max_length=1,
+        # add the 'choices' field option
+        choices=MEALS,
+        # set the default value for meal to be 'B'
+        default=MEALS[0][0]
+    )
+    # Create a cat_id column for each feeding in the database
+    cat = models.ForeignKey(Cat, on_delete=models.CASCADE)
+
+def __str__(self):
+        # Nice method for obtaining the friendly value of a Field.choice
+        return f"{self.get_meal_display()} on {self.date}"
